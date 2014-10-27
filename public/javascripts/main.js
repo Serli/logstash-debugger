26e32c6c13866f435c7d79ef128d0f5c70ddcc21
Nat-Faeeria <Nat-Faeeria@users.noreply.github.com>
2014-10-27 11:17:50 +0100

    implementing dragNDrop

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,16 @@
+var dropZone = document.getElementById("dropBox");
 
+dropZone.ondragover = function(e){
+    e.preventDefault();
+    console.log('dragover');
+}
+
+dropZone.ondrop = function(e){
+    e.preventDefault();
+    
+    if ('files' in e.dataTransfer){
+        dropZone.innerHTML = e.dataTransfer.files[0].name;
+    }else{
+        dropZone.innerHTML = '<p>Ce navigateur ne gère pas le drag\'n drop</p>';
+    }
+}
