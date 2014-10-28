d5448a28a8d16eabe347097691a506d8053667c7
Nat-Faeeria <Nat-Faeeria@users.noreply.github.com>
2014-10-28 10:30:39 +0100

    temporary changes on form

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,25 @@
 package controllers;
 
+import model.forms.LogstashConfiguration;
 import play.*;
 import play.mvc.*;
 
 import views.html.*;
 
+import java.text.Normalizer;
+import java.util.Map;
+
 public class Application extends Controller {
+
+    Form<LogstashConfiguration> configForm = Normalizer.Form.form(LogstashConfiguration.class);
 
     public static Result index() {
         return ok(index.render("Mostach"));
     }
 
+    public static Result submit(){
+        Map<String,Object> anyData = new HashMap();
+        anyData.put("input", "bob@gmail.com");
+        anyData.put("configFile", "secret");
+    }
 }
---
+++ app/model/forms/LogstashConfiguration.java
@@ -0,0 +1,24 @@
+package model.forms;
+
+import java.io.File;
+
+/**
+ * Created by Nat-Faeeria on 27/10/14.
+ */
+public class LogstashConfiguration {
+
+    @Required
+    public String input;
+    @Required
+    public File configFile;
+
+    public String validate(){
+        if (input == null){
+            return "Input is missing";
+        }else if (configFile == null){
+            return ".conf file is missing";
+        }
+        return null;
+    }
+
+}
